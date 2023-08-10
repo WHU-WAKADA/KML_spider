9c6539500993016bf1f3da9738d0f5587a240ea5
WHU-WAKADA <876939618@qq.com>
2023-08-10 13:04:16 +0800

    自动点击错误提示

--- snw.py
+++ snw.py
@@ -3,30 +3,35 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service  # 新增
 import gameme as ga
 import time
+import pyautogui
+import numpy as np
+import cv2
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_experimental_option('excludeSwitches', ['enable-automation'])
 yanzheng = ['../resource/yanzheng.png', '../resource/yanzheng1.png']
 
-for i in range(6, 300):
-
+for i in range(751, 1000):
+    time.sleep(3)
+    print(i)
     # 打开列表啊网页
     url = 'https://www.2bulu.com/track/list-%25E4%25B9%2589%25E4%25B9%258C%25E5%25B8%2582-----'+str(i)+'.htm?sortType=2'#以该链接为例
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
     }
-    service = Service(executable_path='E:/anaconda/envs/game/chromedriver.exe')
+    service = Service(executable_path='C:/ProgramData/anaconda3/envs/game/chromedriver.exe')
     driver = webdriver.Chrome(service=service, options=options)
     driver.set_window_size(1920, 1080)
     driver.get(url)
-    driver.implicitly_wait(0.5)
+    time.sleep(2)
+
 
     if i > 1:
         # 登录
         name = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/ul[2]/li[1]/input")
-        name.send_keys('your name')
+        name.send_keys('18975597126')
         code = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/ul[2]/li[3]/input")
-        code.send_keys('your code')
+        code.send_keys('Aa123456')
         ga.movepng(yanzheng, 0.95, 0.5)
         lo_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/span')
 
@@ -47,9 +52,9 @@ for i in range(6, 300):
         login = driver.find_element(By.XPATH, "/html/body/div[1]/div/div/div[2]/ul/li[4]")
         login.click()
         name = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/ul[2]/li[1]/input")
-        name.send_keys('your name')
+        name.send_keys('18975597126')
         code = driver.find_element(By.XPATH, "/html/body/div[1]/div[1]/ul[2]/li[3]/input")
-        code.send_keys('your code')
+        code.send_keys('Aa123456')
         ga.movepng(yanzheng, 0.95, 0.5)
         lo_button = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/span')
 
@@ -59,7 +64,14 @@ for i in range(6, 300):
     # 下载该数据
     for i in href:
         driver.get(i)
-        time.sleep(2)
+        time.sleep(1)
+        error_location = pyautogui.locateOnScreen('../resource/error.png', confidence=0.95)
+        if error_location is not None:
+            ga.clickpng('../resource/error.png', 0.95, 0.5)
+        save_location = pyautogui.locateOnScreen('../resource/save.png',confidence=0.95)
+        if save_location is not None:
+            ga.clickpng('../resource/save.png', 0.95, 0.5)
+        time.sleep(1)
         ga.clickpng('../resource/download.png', 0.95, 1)
         ga.clickpng('../resource/local.png', 0.95, 1)
         ga.movepng(yanzheng, 0.95, 1)
